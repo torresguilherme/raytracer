8d8079d492c16fab797de3f1be04ac9d63b19a78
Guilherme Torres <guilhermetorres97@gmail.com>
2018-07-08 22:49:14 -0300

    consertei bugs de io

--- src/main.cpp
+++ src/main.cpp
@@ -23,10 +23,15 @@ int main(int argc, char **argv)
 	}
 
 	const char* fileName = argv[1];
-	const char* imageName = argv[2];
+	const char* outName = argv[2];
 
 	// recebe entrada
 	FILE* f = fopen(fileName, "r");	
+	if(f == NULL)
+	{
+		cerr<<"erro: arquivo de entrada nao encontrado"<<endl;
+		exit(2);
+	}
 	float x, y, z;
 	float af;
 	int aux;
@@ -47,7 +52,7 @@ int main(int argc, char **argv)
 	Camera camera(pos, dir, up, af);
 
 	// declara lista de luzes
-	fscanf(f, "%i\n", aux);
+	fscanf(f, "%i\n", &aux);
 	for(int i = 0; i < aux; i++)
 	{
 		fscanf(f, "%f %f %f", &x, &y, &z);
@@ -59,10 +64,10 @@ int main(int argc, char **argv)
 	}
 
 	// recebe pigmentos
-	fscanf(f, "%i\n", aux);
+	fscanf(f, "%i\n", &aux);
 	for(int i = 0; i < aux; i++)
 	{
-		string type;
+		char type[20];
 		fscanf(f, "%s", type);
 		pigms.push_back(Pigm(type));
 		if(type == "solid")
@@ -80,14 +85,14 @@ int main(int argc, char **argv)
 		}
 		else if(type == "texmap")
 		{
-			string imageName;
+			char imageName[30];
 			fscanf(f, "%s\n", imageName);
-			pigms[i].image = fopen(imageName.c_str(), "r");
+			pigms[i].image = fopen(imageName, "r");
 		}
 	}
 
 	// recebe acabamentos
-	fscanf(f, "%i\n", aux);
+	fscanf(f, "%i\n", &aux);
 	for(int i = 0; i < aux; i++)
 	{
 		float a, d, s, se, kr, kt, ior;
@@ -96,10 +101,10 @@ int main(int argc, char **argv)
 	}
 
 	// declara lista de objetos
-	fscanf(f, "%i\n", aux);
+	fscanf(f, "%i\n", &aux);
 	for(int i = 0; i < aux; i++)
 	{
-		string type;
+		char type[20];
 		int p, m;
 		fscanf(f, "%i %i %s", &p, &m, type);
 		if(type == "sphere")
@@ -112,21 +117,38 @@ int main(int argc, char **argv)
 		}
 	}
 
+	// inicia output
+	FILE* out = fopen(outName, "w");
+	fprintf(out, "P3\n%i %i\n255\n", largura, altura);
 
 	// lanca um raio para cada pixel na tela
-	for(int i = 0; i < largura; i++)
+	for(int i = 0; i < altura; i++)
 	{
-		for(int j = 0; j < altura; j++)
+		for(int j = 0; j < largura; j++)
 		{
 			// lanca o raio de visibilidade
+			Vec color;
+
 			// pega o objeto mais proximo que o raio atinge
+			
 			// lanca o raio do objeto a cada fonte de luz
+			
 			// usa luz ambiente
+			
 			// se acerta um objeto, só luz ambiente
+			
 			// se nao, soma cor difusa (ponderado pelo coeficiente
+			
 			// se angulo da normal com direçao a luz < threshold, soma luz especular ponderada pelo coeficiente
+			
 			// se ha coeficiente de reflexao: lança mais raios recursivamente
+			
 			// desenha cor no pixel do arquivo de saida
+			
+			fprintf(out, "%i %i %i\n", (int)color.x, (int)color.y, (int)color.z);
 		}
 	}
+
+	fclose(f);
+	fclose(out);
 }
